af9c96da76c694fa5a436fb03cefcc07565bff78
caketower <dlaqudtn1107@naver.com>
2025-09-01 15:23:08 +0900

    [5강] 글 작성에는 폼을 보여주는 기능과 처리하는 기능 두 가지로 분류된다.

--- src/main/java/com/spring3/domain/post/post/controller/PostController.java
+++ src/main/java/com/spring3/domain/post/post/controller/PostController.java
@@ -1,24 +1,47 @@
 package com.spring3.domain.post.post.controller;
 
+import com.spring3.domain.post.post.entity.Post;
+import com.spring3.domain.post.post.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping("/posts/write")
     @ResponseBody
     public String write() {
 
-
+        //https://localhost:8080/posts/doWrite 로 적어도 되지만 시작과 끝이 localhost8080서버로 같다면 생략가능
         return """
-                <form action="https://localhost:8080/">
+                <form action="/posts/doWrite">
                   <input type="text" name="title">
                   <br>
                   <textarea name="content"></textarea>
+                  <br>
                   <input type="submit" value="작성">
                 </form>
                 """;
     }
+
+    @GetMapping("/posts/doWrite")
+    @ResponseBody
+    public String doWrite(
+            String title,
+            String content
+    ) {
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
+    }
+
 }
